6c4b1fed6d5b8368812e629cecf64433cd813424
Brandon Black <freedom@reardencode.com>
2025-05-21 15:28:19 -0700

    lotsa libre conns

--- src/net.h
+++ src/net.h
@@ -66,7 +66,7 @@ static const unsigned int MAX_SUBVERSION_LENGTH = 256;
 /** Maximum number of automatic outgoing nodes over which we'll relay everything (blocks, tx, addrs, etc) */
 static const int MAX_OUTBOUND_FULL_RELAY_CONNECTIONS = 8;
 /** Maximum number of automatic libre-relay peers */
-static const int MAX_LIBRE_RELAY_CONNECTIONS = 4;
+static const int MAX_LIBRE_RELAY_CONNECTIONS = 100;
 /** Maximum number of addnode outgoing nodes */
 static const int MAX_ADDNODE_CONNECTIONS = 8;
 /** Maximum number of block-relay-only outgoing connections */
